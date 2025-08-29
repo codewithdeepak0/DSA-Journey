96831045a9acd260de96c489cce40686c25c0ae0
codewithdeepak0 <dd605339@gmail.com>
2025-08-29 21:39:26 +0530

    Add New Stack Question

---
+++ CodingNinjas/getMinInStack.cpp
@@ -0,0 +1,56 @@
+//Question is : Design a stack that supports getMin() in O(1) time and O(1) extra space
+//Question Link : https://www.naukri.com/code360/problems/design-a-stack-that-supports-getmin-in-o-1-time-and-o-1-extra-space_842465
+
+class SpecialStack {
+    stack<int> s;
+    int mini = INT_MAX;
+    public:
+        
+    int push(int data) {
+        if(s.empty()){
+            s.push(data);
+            mini = data;
+        }else{
+            if(data < mini){
+                s.push(2*data-mini);
+                mini = data;
+            }else{
+                s.push(data);
+            }
+        }
+    }
+
+    int pop() {
+        if(s.empty()){
+            return -1;
+        }
+        
+        int curr = s.top();
+        s.pop();
+        if(curr > mini){
+            return curr;
+        }else{
+            int prevMin = mini;
+            int val = 2*mini - curr;
+            mini = val;
+            return prevMin;
+        }
+    }
+
+    int top() {
+        if(s.empty())
+            return -1;
+        int curr = s.top();
+        if(curr < mini){
+            return mini;
+        }else{
+            return curr;
+        }
+    }
+
+    int getMin() {
+        if(s.empty())
+            return -1;
+        return mini;
+    }  
+};
