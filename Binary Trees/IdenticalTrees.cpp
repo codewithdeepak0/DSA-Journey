9816fc462b5057e4b3c4334b502b0a7747f77fff
codewithdeepak0 <dd605339@gmail.com>
2025-09-10 11:20:38 +0530

    New Question Add

---
+++ Binary Trees/IdenticalTrees.cpp
@@ -0,0 +1,41 @@
+// Question Link: https://www.geeksforgeeks.org/problems/determine-if-two-trees-are-identical/1
+
+/*
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int x) {
+        data = x;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+    bool isIdentical(Node* root1, Node* root2) {
+        if(root1 == NULL && root2 == NULL){
+            return true;
+        }
+        if(root1 != NULL && root2 == NULL){
+            return false;
+        }
+        if(root1 == NULL && root2 != NULL){
+            return false;
+        }
+        
+        bool left = isIdentical(root1->left, root2->left);
+        bool right = isIdentical(root1->right, root2->right);
+        bool value = root1->data == root2->data;
+        
+        if(left && right && value){
+            return true;
+        }else{
+            return false;
+        }
+    }
+};
